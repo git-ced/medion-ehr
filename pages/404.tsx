bf7ace38ec935d2027b6f4723950056b51020a41
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-12-29 05:17:45 +0800

    refactor: use Geist components instead

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,17 @@
 /* eslint-disable import/no-default-export */
+// ANCHOR Geist
+import { Row, Text } from '@geist-ui/react';
 
 export default function NotFoundPage() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <p className="text-5xl">404 Page Not Found</p>
-    </div>
+    <Row
+      justify="center"
+      align="middle"
+      className="w-full h-screen flex"
+    >
+      <Text b className="text-5xl">
+        404 Page Not Found
+      </Text>
+    </Row>
   );
 }
