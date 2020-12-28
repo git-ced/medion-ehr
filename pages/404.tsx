87aa5d6da74588aff7d9c52e417a625d4b699045
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-12-29 06:46:29 +0800

    chore: use AppHead in NotFoundPage

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,16 +2,25 @@
 // ANCHOR Geist
 import { Row, Text } from '@geist-ui/react';
 
+// ANCHOR Components
+import { AppHead } from '@medion/components/utils/AppHead';
+
 export default function NotFoundPage() {
   return (
-    <Row
-      justify="center"
-      align="middle"
-      className="w-full h-screen flex"
-    >
-      <Text b size="3rem">
-        404 Page Not Found
-      </Text>
-    </Row>
+    <>
+      <AppHead
+        title="Medion | Not Found"
+        description="The page you are looking for does not exist"
+      />
+      <Row
+        justify="center"
+        align="middle"
+        className="w-full h-screen flex"
+      >
+        <Text b size="3rem">
+          404 Page Not Found
+        </Text>
+      </Row>
+    </>
   );
 }
