13e2667ef742ed37ddffd223a7da44bf245a5cbb
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-12-29 20:39:51 +0800

    chore: add firebase config/credentials

---
+++ utils/config/firebase.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
